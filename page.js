c0c24ee0e9cd7e0a5690f17735d0b1a6f671922c
xiaopeng.lei <xiaopeng.lei@gaodun.com>
2021-04-26 19:53:40 +0800

    升级

---
+++ page.js
@@ -0,0 +1,19 @@
+var webpage = require('webpage');
+var system = require('system');
+var page = webpage.create();
+page.onConsoleMessage = function(msg) {
+    console.log(msg);
+};
+var address = system.args[1];
+console.log(address);
+page.open(address, function (status) {
+ if (status === 'fail') {
+  console.log('open page fail!');
+  return;
+ }else{
+    page.includeJs("https://code.jquery.com/jquery-3.6.0.min.js", function() {
+        page.injectJs("getCont.js");
+    });
+    setTimeout(phantom.exit, 10000);
+ }
+});
