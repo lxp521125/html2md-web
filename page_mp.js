4047a0ab0ab82bcd0ca0f8371142a538497ffa2f
xiaopeng.lei <xiaopeng.lei@gaodun.com>
2022-08-18 20:41:37 +0800

    1

--- page_mp.js
+++ page_mp.js
@@ -1,25 +1,12 @@
 var webpage = require('webpage');
 var system = require('system');
 var page = webpage.create();
-var spawn = require("child_process").spawn
 var webPage2 = require('webpage');
 var page2 = webPage2.create();
 
 page.onConsoleMessage = function (msg) {
     var up = JSON.parse(msg);
-    if (up.data) {
-        var settings = {
-            operation: "POST",
-            encoding: "utf8",
-            headers: {
-              "Content-Type": "application/x-www-form-urlencoded"
-            },
-            data: up.data
-          };
-          page2.open(up.url, settings, function(status) {
-            console.log('Status: ' + status);
-        });
-    }    
+    console.log(up);
     if (up.data) {
         var settings = {
             operation: "POST",
@@ -35,12 +22,13 @@ page.onConsoleMessage = function (msg) {
     }
 };
 var address = system.args[1];
+console.log(address);
 page.open(address, function (status) {
     if (status === 'fail') {
         console.log('open page fail!');
         return;
     } else {
         page.injectJs("getCont.js");
-        setTimeout(phantom.exit, 10000);
+        setTimeout(phantom.exit, 20000);
     }
 });
